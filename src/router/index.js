56ed4682e5f63b14b75702c34e5850e79d3789c1
harshvsharma03 <harshvardhan.sharma.che21@itbhu.ac.in>
2024-06-20 13:34:08 +0530

    form

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+// src/router/index.js
+
+import { createRouter, createWebHistory } from 'vue-router';
+import SignIn from '../views/SignIn.vue';
+import Dashboard from '../views/DashBoard.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'SignIn',
+    component: SignIn
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
---
+++ src/views/DashBoard.vue
@@ -0,0 +1,19 @@
+<!-- src/views/Dashboard.vue -->
+
+<template>
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome to the dashboard!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DashBoard'
+  };
+  </script>
+  
+  <style scoped>
+   
+  </style>
+  
